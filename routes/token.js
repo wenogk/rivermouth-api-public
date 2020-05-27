65c5d8837ef7b79119bc057c9dd5631ff723ac27
wenogk <drf325@nyu.edu>
2020-05-27 23:31:01 +0400

    bug

--- routes/token.js
+++ routes/token.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  //res.send('respond with a resource');
   const userID = req.body.userID;
   const user = { name: userID};
 
