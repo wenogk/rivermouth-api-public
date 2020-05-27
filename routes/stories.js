989651e7d8eeed51e8ad3ebbbb3eb45850739bc6
wenogk <drf325@nyu.edu>
2020-05-27 16:52:27 +0400

    cleaning

--- routes/stories.js
+++ routes/stories.js
@@ -2,15 +2,12 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-//Set up default mongoose connection
 var mongoDB = "mongodb+srv://" + process.env.MONGODB_USER_RIVERMOUTH + ":" + process.env.MONGODB_PASS_RIVERMOUTH + "@rivermouth-rt5m7.mongodb.net/test?retryWrites=true&w=majority";
-//mongodb://<dbuser>:<dbpassword>@ds141228.mlab.com:41228/heroku_jr7b7kv0
+
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
-//Get the default connection
 var db = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var Schema = mongoose.Schema;
@@ -22,7 +19,6 @@ var StorySchema = new Schema({
   storyString: String
 });
 
-// Compile model from schema
 var StoryModel = mongoose.model('StoryModel', StorySchema );
 
 var instance = new StoryModel({
@@ -32,14 +28,19 @@ var instance = new StoryModel({
   storyString: "{}sdojodsjosdoiewdhoihewof}"
 });
 
-// Save the new model instance, passing a callback
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   instance.save(function (err) {
     if (err) return handleError(err);
     res.send('saved');
   });
+
+
+router.get('/', function(req, res, next) {
+    instance.save(function (err) {
+      if (err) return handleError(err);
+      res.send('saved');
+    });
 
 });
 
