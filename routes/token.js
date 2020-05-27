2f08e4da67518df1fb06c657fbc4995221a3c8ab
wenogk <drf325@nyu.edu>
2020-05-28 00:50:47 +0400

    google auth stuff

--- routes/token.js
+++ routes/token.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
-
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client("1048507317343-nvfritcgv71asc4ld7lg4gt421grq42j.apps.googleusercontent.com");
+async function verify(userIDVal) { //use this when logging in from react
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: "1048507317343-nvfritcgv71asc4ld7lg4gt421grq42j.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userID = payload['sub'];
+  if (userID==userIDVal) {
+    return true;
+  }
+}
 router.post('/', function(req, res, next) {
   //res.send('respond with a resource');
   const userID = req.body.userID;
