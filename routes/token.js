4cc7c827cde0b8ede6824624d01e19e48afbd390
wenogk <drf325@nyu.edu>
2020-05-28 01:05:22 +0400

    google token validation

--- routes/token.js
+++ routes/token.js
@@ -2,19 +2,22 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client("1048507317343-nvfritcgv71asc4ld7lg4gt421grq42j.apps.googleusercontent.com");
+const GOOGLE_CLIENT_ID ="1048507317343-nvfritcgv71asc4ld7lg4gt421grq42j.apps.googleusercontent.com"; //this is public so no need to hide it
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 async function verify(userIDVal) { //use this when logging in from react
   //https://developers.google.com/identity/sign-in/web/backend-auth read this again (watch video too)
   const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: "1048507317343-nvfritcgv71asc4ld7lg4gt421grq42j.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
+      audience: GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
   const payload = ticket.getPayload();
   const userID = payload['sub'];
-  if (userID==userIDVal) {
-    return true;
+  if ((payload['aud']==GOOGLE_CLIENT_ID)&&((payload['iss']=="https://accounts.google.com")||(payload['iss']=="accounts.google.com"))) {
+    return userID;
+  } else {
+    return null;
   }
 }
 router.post('/', function(req, res, next) {
