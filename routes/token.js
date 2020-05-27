1ddd0facac1d9b30365027bbc1fb8ef0ead88681
wenogk <drf325@nyu.edu>
2020-05-27 23:37:57 +0400

    jwt

--- routes/token.js
+++ routes/token.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var jwt = require('jsonwebtoken');
 
 router.post('/', function(req, res, next) {
   //res.send('respond with a resource');
