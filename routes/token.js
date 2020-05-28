e6d4fb0bb7a06ae3e408cba2a8b1ad13fd2f2a3c
wenogk <drf325@nyu.edu>
2020-05-28 11:19:56 +0400

    test

--- routes/token.js
+++ routes/token.js
@@ -4,24 +4,28 @@ var jwt = require('jsonwebtoken');
 const {OAuth2Client} = require('google-auth-library');
 const GOOGLE_CLIENT_ID ="1048507317343-nvfritcgv71asc4ld7lg4gt421grq42j.apps.googleusercontent.com"; //this is public so no need to hide it
 const client = new OAuth2Client(GOOGLE_CLIENT_ID);
-async function verify(token) { //use this when logging in from react
+function verify(token) { //use this when logging in from react
   //https://developers.google.com/identity/sign-in/web/backend-auth read this again (watch video too)
   console.log("BEFORE TICKET: ");
-  const ticket = await client.verifyIdToken({
+  client.verifyIdToken({
       idToken: token,
       audience: GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  }).then(ticket=> {
+
+      const payload = ticket.getPayload();
+      console.log("PAYLOAD: " + JSON.stringify(payload));
+      const userID = payload['sub'];
+      if ((payload['aud']==GOOGLE_CLIENT_ID)&&((payload['iss']=="https://accounts.google.com")||(payload['iss']=="accounts.google.com"))) {
+        return userID;
+      } else {
+        return null;
+      }
+  }).catch(err=> {
+    console.log("ERROR verifyy(): " + err)
   });
 
-  const payload = ticket.getPayload();
-  console.log("PAYLOAD: " + JSON.stringify(payload));
-  const userID = payload['sub'];
-  if ((payload['aud']==GOOGLE_CLIENT_ID)&&((payload['iss']=="https://accounts.google.com")||(payload['iss']=="accounts.google.com"))) {
-    return userID;
-  } else {
-    return null;
-  }
 }
 router.post('/', function(req, res, next) {
   //res.send('respond with a resource');
